ab95cce7617b81eede13c9a6ecb315daf853178f
Simon Acosta <acostasimon16@gmail.com>
2026-09-11 18:59:57 -0500

    Cleaned up Entitiy object imports

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,15 +1,6 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-
 import { env } from './schemas/env.schema';
-import { Ingredient } from './entities/Ingredient';
-import { IngredientCategory } from './entities/IngredientCategory';
-import { IngredientProduct } from './entities/IngredientProduct';
-import { IngredientUnitConversion } from './entities/IngredientUnitConversion';
-import { ProductPrice } from './entities/ProductPrice';
-import { Retailer } from './entities/Retailer';
-import { StoreLocation } from './entities/StoreLocation';
-import { Unit } from './entities/Unit';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
